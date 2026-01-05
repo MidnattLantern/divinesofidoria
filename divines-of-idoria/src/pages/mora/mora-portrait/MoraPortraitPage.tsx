a04b404a3213e0d65751ff4896b3f7e87896b1fd
Alma <isakvent@icloud.com>
2026-01-05 21:33:17 +0100

    Include download for all mora portrait

--- divines-of-idoria/src/pages/mora/mora-portrait/MoraPortraitPage.tsx
+++ divines-of-idoria/src/pages/mora/mora-portrait/MoraPortraitPage.tsx
@@ -5,6 +5,12 @@ import GalleryCard from "../../../components/GalleryCard/GalleryCard";
 
 import PreviewPortraitLossless from "../../../assets/mora-assets/preview/preview-portrait-lossless.webp";
 import DownloadPortraitLossless from "../../../assets/mora-assets/downloadable/portrait-lossless.png";
+import PreviewPortraitLegacy from "../../../assets/mora-assets/preview/preview-portrait-legacy.webp";
+import DownloadPortraitLegacy from "../../../assets/mora-assets/downloadable/portrait-legacy.png";
+import PreviewPortraitSketch from "../../../assets/mora-assets/preview/preview-portrait-sketch.webp";
+import DownloadPortraitSketch from "../../../assets/mora-assets/downloadable/portrait-sketch.png";
+import PreviewPortraitSketchLegacy from "../../../assets/mora-assets/preview/preview-portrait-sketch-legacy.webp";
+import DownloadPortraitSketchLegacy from "../../../assets/mora-assets/downloadable/portrait-sketch-legacy.png";
 
 function DisplayMoraPortraitPage() {
     useEffect(() => {
@@ -12,13 +18,38 @@ function DisplayMoraPortraitPage() {
     }, []);
 
     return (
-        <section>
-            <h2>Mora Goddess — Portrait</h2>
-            <GalleryCard
-            title="Lossless"
-            previewSource={PreviewPortraitLossless}
-            downloadSource={DownloadPortraitLossless}
-            />
+        <section className="gallery-view">
+            <h1><span>Mora Goddess</span><span>Portrait</span></h1>
+            <ul>
+                <li>
+                    <GalleryCard
+                    title="Lossless"
+                    previewSource={PreviewPortraitLossless}
+                    downloadSource={DownloadPortraitLossless}
+                    />
+                </li>
+                <li>
+                    <GalleryCard
+                    title="Legacy"
+                    previewSource={PreviewPortraitLegacy}
+                    downloadSource={DownloadPortraitLegacy}
+                    />
+                </li>
+                <li>
+                    <GalleryCard
+                    title="Sketch"
+                    previewSource={PreviewPortraitSketch}
+                    downloadSource={DownloadPortraitSketch}
+                    />
+                </li>
+                <li>
+                    <GalleryCard
+                    title="Sketch Legacy"
+                    previewSource={PreviewPortraitSketchLegacy}
+                    downloadSource={DownloadPortraitSketchLegacy}
+                    />
+                </li>
+            </ul>
         </section>
     );
 };
