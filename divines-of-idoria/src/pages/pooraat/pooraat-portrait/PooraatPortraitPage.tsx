b7fc59eaeb0c32cd8cc1f7a99f057552a41248a2
Alma <isakvent@icloud.com>
2026-02-04 20:16:59 +0100

    add all 5 portrait assets to UI

--- divines-of-idoria/src/pages/pooraat/pooraat-portrait/PooraatPortraitPage.tsx
+++ divines-of-idoria/src/pages/pooraat/pooraat-portrait/PooraatPortraitPage.tsx
@@ -4,13 +4,41 @@ import Gallery from "../../../components/Gallery/Gallery";
 import { Route, Routes } from "react-router";
 import PreviewLossless from "../../../assets/pooraat-assets/preveiw/portrait-lossless-preview.webp";
 import DownloadLossless from "../../../assets/pooraat-assets/downloadable/portrait-lossless.png";
+import PreviewNoFilter from "../../../assets/pooraat-assets/preveiw/portrait-no-filter-preview.webp";
+import DownloadNoFilter from "../../../assets/pooraat-assets/downloadable/portrait-no-filter.png";
+import PreviewFramed from "../../../assets/pooraat-assets/preveiw/portrait-framed-preview.webp";
+import DownloadFramed from "../../../assets/pooraat-assets/downloadable/portrait-framed.png";
+import PreviewFramedNoFilter from "../../../assets/pooraat-assets/preveiw/framed-no-filter-preview.webp";
+import DownloadFramedNoFilter from "../../../assets/pooraat-assets/downloadable/portrait-no-filter-framed.png";
+import PreviewCutout from "../../../assets/pooraat-assets/preveiw/portrait-cutout-preview.webp";
+import DownloadCutout from "../../../assets/pooraat-assets/downloadable/portrait-cutout.png";
 
 const PooratPortraitItems = [
     {
         title: "Lossless",
         previewSource: PreviewLossless,
         downloadSource: DownloadLossless
-    }
+    },
+    {
+        title: "No filter",
+        previewSource: PreviewNoFilter,
+        downloadSource: DownloadNoFilter
+    },
+    {
+        title: "Framed",
+        previewSource: PreviewFramed,
+        downloadSource: DownloadFramed
+    },
+    {
+        title: "Framed no filter",
+        previewSource: PreviewFramedNoFilter,
+        downloadSource: DownloadFramedNoFilter
+    },
+    {
+        title: "Cutout",
+        previewSource: PreviewCutout,
+        downloadSource: DownloadCutout
+    },
 ]
 
 function DisplayPooraatPortraitPage() {
