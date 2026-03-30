ac4f33478ee8ffe090642fab235639ca1c8814d5
Alma <isakvent@icloud.com>
2026-03-30 21:06:40 +0200

    (feature): Import sunbawn symbol assets

--- divines-of-idoria/src/pages/sunbawn/sunbawn-symbol/SunbawnSymbolPage.tsx
+++ divines-of-idoria/src/pages/sunbawn/sunbawn-symbol/SunbawnSymbolPage.tsx
@@ -1,4 +1,9 @@
 import "./SunbawnSymbolPage.scss";
+import PreviewSymbolLossless from "../../../assets/sunbawn-assets/preview/preview-symbol.webp";
+import DownloadSymbolLossless from "../../../assets/sunbawn-assets/downloadable/symbol.png";
+import DownloadSymbolCompressed from "../../../assets/sunbawn-assets/downloadable/symbol-compressed.jpg";
+import PreviewSymbolCutout from "../../../assets/sunbawn-assets/preview/preview-symbol-cutout.webp";
+import DownloadSymbolCutout from "../../../assets/sunbawn-assets/downloadable/symbol-cutout.png";
 import PreviewVectorSilhouette from "../../../assets/sunbawn-assets/preview/preview-symbol-vector-silhouette.webp";
 import DownloadVectorSilhouette from "../../../assets/sunbawn-assets/downloadable/symbol-vector-silhouette.svg";
 import PreviewVectorOutline from "../../../assets/sunbawn-assets/preview/preview-symbol-vector-outline.webp";
@@ -8,6 +13,19 @@ import Gallery from "../../../components/Gallery/Gallery";
 import { Route, Routes } from "react-router";
 
 const SunbawnSymbolItems = [
+    {
+        title: "Original",
+        previewSource: PreviewSymbolLossless,
+        downloadSource: DownloadSymbolLossless,
+        useWideAspectRatio: true,
+        compressedDownloadSource: DownloadSymbolCompressed
+    },
+    {
+        title: "Cutout",
+        previewSource: PreviewSymbolCutout,
+        downloadSource: DownloadSymbolCutout,
+        useWideAspectRatio: true,
+    },
     {
         title: "Vector Silhouette",
         previewSource: PreviewVectorSilhouette,
