da1ba7cebaf07c45d7e0ffd07e437879dc7fb450
Alma <isakvent@icloud.com>
2026-05-14 16:35:37 +0200

    (feature): document the making of po'oraat symbol

--- divines-of-idoria/src/pages/pooraat/pooraat-symbol/PooraatSymbolPage.tsx
+++ divines-of-idoria/src/pages/pooraat/pooraat-symbol/PooraatSymbolPage.tsx
@@ -1,10 +1,12 @@
 import "./PooraatSymbolPage.css";
 import { useEffect } from "react";
+import projectsDNA from "./projectsDNA.json";
 import Gallery from "../../../components/Gallery/Gallery";
 import PreviewFiltered from "../../../assets/pooraat-assets/preveiw/preview-filtered.webp";
 import DownloadFiltered from "../../../assets/pooraat-assets/downloadable/filtered.png";
 import DownloadCompressed from "../../../assets/pooraat-assets/downloadable/symbol-comporessed.jpg";
 import { Route, Routes } from "react-router";
+import ProjectsDNA from "../../../components/projectsDNA/projectsDNA";
 
 const PooraatSymbolItems = [
     {
@@ -21,7 +23,10 @@ function DisplayPooraatSymbolPage() {
     }, []);
 
     return (
-        <Gallery deity="Po'oraat" projectName="Symbol" items={PooraatSymbolItems}/>
+        <>
+            <Gallery deity="Po'oraat" projectName="Symbol" items={PooraatSymbolItems}/>
+            <ProjectsDNA projectName="Po'oraat symbol" projectsDNA={projectsDNA}/>
+        </>
     );
 }
 
