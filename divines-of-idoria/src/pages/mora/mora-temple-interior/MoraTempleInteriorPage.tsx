ef94be6eac769848751345a0d25c00f8b3490235
Alma <isakvent@icloud.com>
2026-08-13 15:21:30 +0200

    (feature): Include documentation behind the making of mora temple interior

--- divines-of-idoria/src/pages/mora/mora-temple-interior/MoraTempleInteriorPage.tsx
+++ divines-of-idoria/src/pages/mora/mora-temple-interior/MoraTempleInteriorPage.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import "./MoraTempleInteriorPage.css";
 import Gallery from "../../../components/Gallery/Gallery";
 import { Route, Routes } from "react-router";
 import PreviewMoraTempleInteriorLossless from "../../../assets/mora-assets/preview/preview-temple-interior.webp";
 import DownloadMoraTempleInteriorLossless from "../../../assets/mora-assets/downloadable/temple-interior.png";
 import DownloadMoraTempleInteriorCompressed from "../../../assets/mora-assets/downloadable/temple-interior-compressed.jpg";
+import ProjectsDNA from "../../../components/projectsDNA/projectsDNA";
+import ProjectsDNAData from "./projectsDNA.json";
 
 const MoraTempleInteriorItems = [
     {
@@ -22,7 +23,10 @@ function DisplayMoraTempleInteriorPage() {
     }, []);
 
     return (
-        <Gallery deity="Mora" projectName="Temple Interior" items={MoraTempleInteriorItems}/>
+        <>
+            <Gallery deity="Mora" projectName="Temple Interior" items={MoraTempleInteriorItems}/>
+            <ProjectsDNA projectName="Mora Temple Interior" projectsDNA={ProjectsDNAData}/>
+        </>
     );
 };
 
