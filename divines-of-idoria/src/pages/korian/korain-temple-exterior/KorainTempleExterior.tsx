0542f700f64decfde96526974a846f5d949b3883
Alma <isakvent@icloud.com>
2026-08-13 17:58:55 +0200

    (feature) Create Korain exterior temple documentation

--- divines-of-idoria/src/pages/korian/korain-temple-exterior/KorainTempleExterior.tsx
+++ divines-of-idoria/src/pages/korian/korain-temple-exterior/KorainTempleExterior.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router";
 import Gallery from "../../../components/Gallery/Gallery";
-import "./KorainTempleExterior.css";
 import PreviewKorainTempleExteriorLossless from "../../../assets/korain-assets/preview/preview-temple-exterior-lossless.webp";
 import DownloadKorainTempleExteriorLossless from "../../../assets/korain-assets/downloadable/temple-exterior-lossless.png";
 import DownloadKorainTempleExteriorCompressed from "../../../assets/korain-assets/downloadable/temple-exterior-compressed.jpg";
+import ProjectsDNA from "../../../components/projectsDNA/projectsDNA";
+import ProjectsDNAData from "./projectsDNA.json";
 
 const KorainTempleExteriorItems = [
     {
@@ -17,7 +18,10 @@ const KorainTempleExteriorItems = [
 
 function DisplayKorainTempleExterior() {
     return (
-        <Gallery deity="Korain" projectName="Temple Exterior" items={KorainTempleExteriorItems}/>
+        <>
+            <Gallery deity="Korain" projectName="Temple Exterior" items={KorainTempleExteriorItems}/>
+            <ProjectsDNA projectName="Korain Temple Exterior" projectsDNA={ProjectsDNAData}/>
+        </>
     );
 };
 
