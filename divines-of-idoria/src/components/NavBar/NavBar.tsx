f0c03aebad2c5ae2363c18b350203a73782f9f97
Alma <isakvent@icloud.com>
2026-08-16 19:26:53 +0200

    (feature) Include icons

--- divines-of-idoria/src/components/NavBar/NavBar.tsx
+++ divines-of-idoria/src/components/NavBar/NavBar.tsx
@@ -2,6 +2,10 @@ import { NavLink } from "react-router";
 import "./NavBar.scss";
 import { useState } from "react";
 import BurgerMenu from "../../assets/vector-icons/burger-menu.svg?react";
+import MoraSVGSymbol from "../../assets/vector-icons/mora-symbol.svg?react";
+import KorainSVGSymbol from "../../assets/vector-icons/korain-symbol.svg?react";
+import PooraatSVGSymbol from "../../assets/vector-icons/pooraat-symbol.svg?react";
+import SunbawnSVGSymbol from "../../assets/vector-icons/sunbawn-symbol-silhouette.svg?react";
 
 function NavBar() {
     const [showNavBar, setShowNavBar] = useState<boolean>(false);
@@ -26,31 +30,51 @@ function NavBar() {
             </button>
                 <nav className={showNavBar ? "header-is-visible" : "header-is-hidden"} id="primaryNavigationBar" aria-label="Primary navigation bar">
                     <div className="navbar-logo">
-                        <NavLink onClick={handleHideNavBar} to={"/"} end>Divines Of Idoria</NavLink>
+                        <NavLink className={"justify-to-center"} onClick={handleHideNavBar} to={"/"} end>
+                            <span>Divines Of Idoria</span>
+                        </NavLink>
                     </div>
                     <ul>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/mora-goddess"}>Mora</NavLink>
+                            <NavLink onClick={handleHideNavBar} to={"/mora-goddess"}>
+                                <MoraSVGSymbol/>
+                                <span>Mora</span>
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/korain-god"}>Korain</NavLink>
+                            <NavLink onClick={handleHideNavBar} to={"/korain-god"}>
+                            <KorainSVGSymbol/>
+                                <span>Korain</span>
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/pooraat-god"}>Po'oraat</NavLink>
+                            <NavLink onClick={handleHideNavBar} to={"/pooraat-god"}>
+                            <PooraatSVGSymbol/>
+                                <span>Po'oraat</span>
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/sunbawn-god"}>Sunbawn</NavLink>
+                            <NavLink onClick={handleHideNavBar} to={"/sunbawn-god"}>
+                                <SunbawnSVGSymbol/>
+                                <span>Sunbawn</span>
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/idor-god"}>Idor</NavLink>
+                            <NavLink className={"justify-to-center"} onClick={handleHideNavBar} to={"/idor-god"}>
+                                <span>Idor</span>
+                            </NavLink>
                         </li>
                     </ul>
                     <ul className="bottom-section">
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/"} end>Home</NavLink>
+                            <NavLink className={"justify-to-center"} onClick={handleHideNavBar} to={"/"} end>
+                                <span>Home</span>
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink onClick={handleHideNavBar} to={"/about"} end>About</NavLink>
+                            <NavLink className={"justify-to-center"} onClick={handleHideNavBar} to={"/about"} end>
+                                <span>About</span>
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
